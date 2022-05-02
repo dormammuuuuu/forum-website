048e3e9e1aa402faa01f75ed370eee22cf0ca465
dormammuuuuu <78258443+dormammuuuuu@users.noreply.github.com>
2022-05-02 20:06:19 +0800

    Create thread static UI

---
+++ javascript/create.js
@@ -0,0 +1,22 @@
+$(function () {
+    $(".guide-expanded").hide();
+    var globalGuide = 1;
+    $('.guide')[0].click(); 
+});
+
+var globalGuide = 0;
+
+$('.guide').click(function () {
+    let current = $(this).attr("data-number");
+    if(current != globalGuide){
+        $(".guide-expanded").slideUp();
+        $(this).children(".guide-expanded").slideDown();
+        globalGuide = current;
+    } else {
+        $(this).children(".guide-expanded").slideUp();
+        globalGuide = current;
+        if ($(this).children(".guide-expanded").css("display") == "none"){
+            $(this).children(".guide-expanded").slideDown();
+        }
+    }
+});
