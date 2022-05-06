a339cf3a65cc3342c190bd52b84dec7a11c3231f
dormammuuuuu <78258443+dormammuuuuu@users.noreply.github.com>
2022-05-06 22:32:50 +0800

    Google OAuth2 API

--- php-scripts/vendor/composer/autoload_real.php
+++ php-scripts/vendor/composer/autoload_real.php
@@ -52,6 +52,29 @@ class ComposerAutoloaderInitc1564135c8ebe3e983489fd02f9d50b4
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitc1564135c8ebe3e983489fd02f9d50b4::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirec1564135c8ebe3e983489fd02f9d50b4($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequirec1564135c8ebe3e983489fd02f9d50b4($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
