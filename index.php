b2be45e9e69ce518159915e5329d1560951abeb3
Francis14-web <francispanaligan54@gmail.com>
2022-05-28 01:02:23 +0800

    LANDING PAGE

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Landing Page</title>
+        <link rel="stylesheet" href="styles/default.css">
+        <link rel="stylesheet" href="styles/index.css">
+        <link rel="stylesheet" href="styles/navbar.css">
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    </head>
+    <body>
+        
+       
+
+        <section class="main"> 
+           <div class="container">
+                <div class="first-div">
+                   <div class="main-intro">
+                        <h1 id="main-title">TUP Speak</h1>
+                        <p>TUP-Speak is a free forum website for TUP students and faculty members. It contains communication, teamwork, and a healthy environment</p>
+                        <button type="button" id="btn_start">Get Started!</button>
+                    </div>
+
+                   <div class="main-img">
+                   </div>                 
+                </div>
+
+                <div class="second-div">
+                    <div class="second-obj-desc">
+                        <div class="cards">
+                            <img src="assets/images/sending-thread.svg" class="card-img">
+                            <h1>Creating Threads</h1>
+                            <p>Share your ideas</p>
+                        </div>    
+
+                        <div class="cards">
+                            <img src="assets/images/thread-proc.svg" class="card-img">
+                            <h1>Admin's Approval</h1>
+                            <p>Wait for the admin's approval for your created threads</p>
+                        </div>    
+
+                        <div class="cards">
+                            <img src="assets/images/approve.svg" class="card-img">
+                            <h1>Thread Published</h1>
+                            <p>Let the other hear your concern</p>
+                        </div>    
+
+                    </div>
+                </div>
+
+                <div class="third-div">
+                    <div class="third-obj">                       
+                        <img src="assets/images/comm.svg" id="comm-img"">               
+                    </div>
+
+                    <div class="main-obj-desc">
+                        <h1 class="obj-title">Communication</h1>
+                            <p class="obj-desc">Participants gain emotionally and academically from their connections with their peers by contributing to others, developing new friends and contacts, staying update on current events, and learning about new possibilities.</p>
+                    </div>                    
+                </div>
+            
+                <div class="fourth-div">
+                    <div class="main-obj-desc">
+                        <h1 class="obj-title">Teamwork</h1>
+                        <p class="obj-desc">Members are allowed to share their files about their reviewers, studies, lectures, and other knowledge to help others so that they are able to grow and learn that may help them to be ready for their career in the near future. They have complete freedom to use facts to educate one another in a rightful manner.</p>
+                    </div>
+
+                    <div class="fourth-obj">
+                      <img src="assets/images/teamwork.svg" id="teamwork-img">
+                    </div>
+                </div>
+
+                <div class="fifth-div">
+                    <div class="fifth-obj">
+                        <img src="assets/images/friendly.svg" id="friendly-img">
+                    </div>
+                
+                    <div class="main-obj-desc">
+                            <h1 class="obj-title">Healthy Environment</h1>
+                            <p class="obj-desc">TUP is one of the best engineering schools that offers affordable tuition fees and quality education. All of the people under the university are either professional or learning to be come professional, and also at the right age to control themselves, to act right in any situation that builds an effective healthy environment for tup speak forum.</p>                   
+                    </div>
+                </div>
+
+                <div class="status-div">
+                    <div class="status-container">
+
+                    </div>
+                </div>
+
+            </div>    
+        </section>
+    </body>
+</html>
